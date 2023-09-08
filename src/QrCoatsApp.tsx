4dc44b61b84949478b4b8dbf3bb4dcbac3910c64
eusebio <eusebio@eusebios-MacBook-Air.local>
2023-09-08 16:17:55 -0600

    firts commit

---
+++ src/QrCoatsApp.tsx
@@ -0,0 +1,10 @@
+
+const QrCoatsApp = () => {
+  return (
+    <>
+    <h1>hola mundo</h1>
+    </>
+  )
+}
+
+export default QrCoatsApp
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./styles.css";
+import QrCoatsApp from "./QrCoatsApp";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QrCoatsApp />
+  </React.StrictMode>
+);
